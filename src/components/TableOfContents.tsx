79b08df8721ba0122c96e4aac0f6f3e24d987136
sungpaks <emforhs03150@gmail.com>
2024-06-09 22:56:38 +0900

    [fix] : 이벤트 핸들러 삭제 추가

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -7,13 +7,16 @@ interface ComponentProps {
 
 function TableOfContents({ tableOfContents }: ComponentProps) {
   const [isWide, setIsWide] = useState<boolean | undefined>(true)
+
   useEffect(() => {
     const postSection = document.getElementById("post-section")
     if (!postSection) return
+    //본문에서 제목 요소들을 가져온다
     const headers = postSection.querySelectorAll("h1, h2, h3, h4, h5, h6")
     if (!headers) return
 
-    document.addEventListener("scroll", () => {
+    //스크롤 이벤트 정의
+    const scrollEvent = () => {
       const overTheTop: Element[] = []
       headers.forEach(h => {
         if (h.getBoundingClientRect().top < 0) {
@@ -27,10 +30,16 @@ function TableOfContents({ tableOfContents }: ComponentProps) {
       aTags?.forEach(a => {
         a.classList.remove("activated")
         if (a.textContent === curHeaderText) {
+          console.info(a)
           a.classList.add("activated")
         }
       })
-    })
+    }
+    //스크롤 이벤트 등록
+    document.addEventListener("scroll", scrollEvent)
+    return () => {
+      document.removeEventListener("scroll", scrollEvent)
+    }
   }, [])
 
   useEffect(() => {
