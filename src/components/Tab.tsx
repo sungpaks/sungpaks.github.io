7a090e268f13d5e5c671eb95c5e170508b2dd401
sungpaks <emforhs03150@gmail.com>
2024-09-21 17:55:56 +0900

    [fix] 탭에서 버튼 대신 Link사용

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,3 +1,4 @@
+import { Link } from "gatsby";
 import React from "react"
 
 interface TabPropsType {
@@ -9,22 +10,25 @@ interface TabPropsType {
 export default function Tab({ onClickAnother, amount, curTab} : TabPropsType) {
 	return (
 		<div className="tab-container">
-			<button 
+			<Link className={curTab === 0 ? "tab-item active" : "tab-item"} to="/">POST {curTab === 0 ? <span className="amount">{`${amount}`}</span> : undefined}</Link>
+			
+			{/* <button 
 			className={curTab === 0 ? "tab-item active" : "tab-item"}
 			onClick={()=>{
 				if(curTab === 1) onClickAnother();
 			}}
 			>
 				POST {curTab === 0 ? <span className="amount">{`${amount}`}</span> : undefined}
-			</button>
-			<button 
+			</button> */}
+			<Link to="/til" className={curTab === 1 ? "tab-item active" : "tab-item"}>TIL {curTab === 1 ? <span className="amount">{`${amount}`}</span> : undefined}</Link>
+			{/* <button 
 			className={curTab === 1 ? "tab-item active" : "tab-item"}
 			onClick={()=>{
 				if(curTab === 0) onClickAnother();
 			}}
 			>
-				TIL {curTab === 1 ? <span className="amount">{`${amount}`}</span> : undefined}
-			</button>
+				
+			</button> */}
 		</div>
 	)
 }
