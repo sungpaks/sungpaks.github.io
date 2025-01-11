792ab4e58cc29dfb0a7f8ee6ebed71cd75f2b1b3
sungpaks <emforhs03150@gmail.com>
2025-01-11 23:41:12 +0900

    feat: r3f 데모

---
+++ src/components/DynamicParticleSystem.tsx
@@ -0,0 +1,137 @@
+import React, { useCallback, useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import { Points, Vector3 } from "three";
+
+const PARTICLE_LIFETIME = 3000; // ms 단위로 파티클 생명주기
+const SPAWN_RATE = 10; // 매 프레임마다 생성되는 파티클 수
+const REMOVE_PROBABILITY = 0.05; // 제거 확률
+
+function DynamicParticleSystem({
+  color,
+  dust
+}: {
+  color: string;
+  dust?: boolean;
+}) {
+  const particles = React.useRef<Points>(null);
+  const trailCount = 5; // 잔상 레이어 수
+  const getRandomPositionFromCenter = useCallback(
+    () => Math.random() * 1.4 - 0.7,
+    []
+  );
+
+  const count = 1000; // 파티클 수
+  const particlesData = React.useRef({
+    positions: new Float32Array(count * 3),
+    velocities: new Float32Array(count * 3)
+  });
+
+  // 잔상 배열
+  const trailLayers = React.useRef(
+    Array.from({ length: trailCount }, () => ({
+      positions: new Float32Array(count * 3),
+      opacity: 0.6 // 기본 투명도
+    }))
+  );
+
+  // 초기화
+  for (let i = 0; i < count; i++) {
+    const distance = 5 + Math.random() * 5;
+    const x = dust ? (Math.random() - 0.9) * 4 : (Math.random() - 0.5) * 2;
+    const y = dust ? (Math.random() - 0.9) * 4 : (Math.random() - 0.5) * 2;
+    const z = Math.cos(Math.PI * 2) * -distance;
+
+    particlesData.current.positions[i * 3] = getRandomPositionFromCenter();
+    particlesData.current.positions[i * 3 + 1] = getRandomPositionFromCenter();
+    particlesData.current.positions[i * 3 + 2] = getRandomPositionFromCenter();
+    particlesData.current.velocities[i * 3] = x * 0.01;
+    particlesData.current.velocities[i * 3 + 1] = y * 0.01;
+    particlesData.current.velocities[i * 3 + 2] = z * 0.01;
+  }
+
+  useFrame(() => {
+    if (!particles.current) return;
+
+    const positions = particlesData.current.positions;
+    const velocities = particlesData.current.velocities;
+
+    // 잔상 레이어 업데이트
+    for (let t = trailCount - 1; t > 0; t--) {
+      trailLayers.current[t].positions.set(
+        trailLayers.current[t - 1].positions
+      );
+      trailLayers.current[t].opacity -= 0.1; // 잔상 투명도 점점 줄이기
+    }
+
+    // trailLayers[0]에 현재 파티클의 위치 저장
+    trailLayers.current[0].positions.set(positions);
+    trailLayers.current[0].opacity = 0.6;
+
+    for (let i = 0; i < count; i++) {
+      const particlePosition = new Vector3(
+        positions[i * 3],
+        positions[i * 3 + 1],
+        positions[i * 3 + 2]
+      );
+
+      particlePosition.x += velocities[i * 3];
+      particlePosition.y += velocities[i * 3 + 1];
+      particlePosition.z += velocities[i * 3 + 2];
+
+      const dist = particlePosition.length();
+      const maxDistance = 500;
+      const minProbability = 0.01;
+      const maxProbability = 0.99;
+      const probability =
+        Math.pow(dist / maxDistance, 2) * (maxProbability - minProbability) +
+        minProbability;
+
+      if (Math.random() < probability / 2) {
+        positions[i * 3] = getRandomPositionFromCenter();
+        positions[i * 3 + 1] = getRandomPositionFromCenter();
+        positions[i * 3 + 2] = getRandomPositionFromCenter();
+      } else {
+        positions[i * 3] = particlePosition.x;
+        positions[i * 3 + 1] = particlePosition.y;
+        positions[i * 3 + 2] = particlePosition.z;
+      }
+    }
+    particles.current.geometry.attributes.position.needsUpdate = true;
+  });
+
+  return (
+    <>
+      <points ref={particles}>
+        <bufferGeometry>
+          <bufferAttribute
+            attach="attributes-position"
+            count={count}
+            array={particlesData.current.positions}
+            itemSize={3}
+          />
+        </bufferGeometry>
+        <pointsMaterial size={0.1} color={color} transparent opacity={0.8} />
+      </points>
+      {trailLayers.current.map((trail, index) => (
+        <points key={index}>
+          <bufferGeometry>
+            <bufferAttribute
+              attach="attributes-position"
+              count={count}
+              array={trail.positions}
+              itemSize={3}
+            />
+          </bufferGeometry>
+          <pointsMaterial
+            size={0.1}
+            color={color}
+            transparent
+            opacity={trail.opacity}
+          />
+        </points>
+      ))}
+    </>
+  );
+}
+
+export default DynamicParticleSystem;
--- src/pages/r3f-demo.tsx
+++ src/pages/r3f-demo.tsx
@@ -1,17 +1,32 @@
-import { Canvas, MeshProps, Object3DNode, useFrame } from "@react-three/fiber";
-import React from "react";
-import { Mesh } from "three";
+import {
+  Canvas,
+  MeshProps,
+  Object3DNode,
+  useFrame,
+  useLoader,
+  useThree
+} from "@react-three/fiber";
+import React, { Suspense, useCallback } from "react";
+import { Color, Mesh, Points, TextureLoader } from "three";
+import Layout from "../components/Layout";
+// @ts-ignore
+import ceres from "../images/ceres.jpg"; // @ts-ignore
+import sun from "../images/sun.jpg"; // @ts-ignore
+import starBackground from "../images/stars-background.jpg";
 
-export default function R3FDemo() {
+export default function R3FDemo({ location }: { location: Location }) {
   return (
-    <>
-      <h1>hi~ This is R3F Demo Page</h1>
+    <Layout location={location}>
       <div
         id="canvas-container"
         style={{
           border: "0.5px solid gray",
-          height: "500px",
-          backgroundColor: "black"
+          height: "100vh",
+          backgroundColor: "black",
+          position: "fixed",
+          width: "100vw",
+          top: 0,
+          left: 0
         }}
       >
         <Canvas
@@ -19,34 +34,42 @@ export default function R3FDemo() {
             fov: 75,
             near: 0.1,
             far: 1000,
-            position: [0, 20, 20]
+            position: [0, 50, 75]
             // lookAt: () => [0, 0, 0]
           }}
         >
-          <ambientLight />
-          <directionalLight position={[10, 10, 10]} />
+          <ambientLight intensity={1} />
+          {/* <directionalLight position={[10, 10, 10]} /> */}
           <Sun />
-          <mesh position={[0, -5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-            <planeGeometry args={[500, 500]} />
+          {/* <mesh position={[0, -25, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+            <planeGeometry args={[1000, 1000]} />
             <meshStandardMaterial color={"rgb(50,50,50)"} />
-          </mesh>
+          </mesh> */}
         </Canvas>
       </div>
-    </>
+    </Layout>
   );
 }
 
 function Sun() {
   const mesh = React.useRef<Mesh>(null);
-  useFrame(({ clock }) => {
-    if (!mesh.current) return;
-    const elapsedTime = clock.getElapsedTime();
-    mesh.current.rotation.y = elapsedTime / 2;
+  const background = useLoader(TextureLoader, starBackground);
+  const sunTexture = useLoader(TextureLoader, sun);
+
+  useThree(({ scene }) => {
+    scene.background = background;
   });
+
   return (
     <mesh ref={mesh}>
-      <sphereGeometry args={[2, 32, 32]} />
-      <meshStandardMaterial args={[{ color: "yellow" }]} />
+      <sphereGeometry args={[5, 32, 32]} />
+      <meshPhongMaterial
+        args={[
+          {
+            map: sunTexture
+          }
+        ]}
+      />
       <pointLight position={[0, 0, 0]} intensity={1000} />
       <SunSystem />
     </mesh>
@@ -56,7 +79,9 @@ function Sun() {
 function SunSystem(props: MeshProps) {
   return (
     <mesh {...props}>
-      <Planet position={[10, 0, 0]} />
+      <Suspense fallback={null}>
+        <Planet position={[30, 0, 0]} />
+      </Suspense>
     </mesh>
   );
 }
@@ -64,6 +89,34 @@ function SunSystem(props: MeshProps) {
 function Planet(props: MeshProps) {
   const [isClicked, setIsClicked] = React.useState(false);
   const mesh = React.useRef<Mesh>(null);
+  const [distanceToCenter, setDistanceToCenter] = React.useState(0);
+
+  const texture = useLoader(TextureLoader, ceres);
+
+  // 궤도 파라미터 설정
+  const semiMajorAxis = 60; // 장축 반지름 (x축 방향)
+  const semiMinorAxis = 20; // 단축 반지름 (y축 방향)
+  const orbitCenter = [semiMajorAxis / 2, 0, 0]; // 중심점
+  const orbitSpeed = 0.5; // 속도
+
+  useFrame(({ clock }) => {
+    if (!mesh.current) return;
+    const elapsedTime = clock.getElapsedTime() * orbitSpeed; // 시간에 따라 궤도 위치 갱신
+    mesh.current.lookAt(0, 0, 0); // 중심을 바라보도록 설정
+
+    // 타원 궤도 좌표 계산
+    const x = semiMajorAxis * Math.cos(elapsedTime); // x축 좌표
+    const z = semiMinorAxis * Math.sin(elapsedTime); // z축 좌표
+
+    mesh.current.position.set(
+      orbitCenter[0] + x,
+      orbitCenter[1],
+      orbitCenter[2] + z
+    );
+    // setDistanceToCenter(
+    //   Math.sqrt(mesh.current.position.x ** 2 + mesh.current.position.z ** 2)
+    // );
+  });
 
   return (
     <mesh
@@ -72,8 +125,101 @@ function Planet(props: MeshProps) {
       scale={isClicked ? 1.5 : 1}
       {...props}
     >
+      <axesHelper args={[5]} />
       <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial args={[{ color: "green" }]} />
+      <meshStandardMaterial args={[{ map: texture }]} />
+      <ParticleSystem distanceToCenter={distanceToCenter} color="#7DF9FF" />
+      <ParticleSystem
+        distanceToCenter={distanceToCenter}
+        dust
+        color="#FFD700"
+      />
+      <ParticleSystem
+        distanceToCenter={distanceToCenter}
+        dust
+        color="#FF4500"
+      />
+      <ParticleSystem distanceToCenter={distanceToCenter} color="#6457A6" />
     </mesh>
+  );
+}
+
+function ParticleSystem({
+  color,
+  dust,
+  distanceToCenter
+}: {
+  color: string;
+  dust?: boolean;
+  distanceToCenter: number;
+}) {
+  const particles = React.useRef<Points>(null);
+  const getRandomPositionFromCenter = useCallback(() => {
+    return Math.random() * 2 - 1;
+  }, []);
+
+  const count = 1000; // 파티클 수
+  const particlesData = React.useMemo(() => {
+    const positions = [];
+    const velocities = [];
+    for (let i = 0; i < count; i++) {
+      const distance = 5 + Math.random() * 5; // 중심으로부터 거리
+      const x = dust ? (Math.random() - 0.9) * 4 : (Math.random() - 0.5) * 2;
+      const y = dust ? (Math.random() - 0.9) * 4 : (Math.random() - 0.5) * 2;
+      const z = Math.cos(Math.PI * 2) * -distance; // 높이 축 랜덤
+      positions.push(
+        getRandomPositionFromCenter(),
+        getRandomPositionFromCenter(),
+        getRandomPositionFromCenter()
+      ); // 위치 설정 );
+      velocities.push(x * 0.01, y * 0.01, z * 0.01); // 속도 설정
+    }
+    return { positions, velocities };
+  }, []);
+  useFrame(() => {
+    if (!particles.current) return;
+
+    const positions = particles.current.geometry.attributes.position.array;
+    for (let i = 0; i < count; i++) {
+      positions[i * 3] += particlesData.velocities[i * 3] - (dust ? 0.01 : 0); // x
+      positions[i * 3 + 1] += particlesData.velocities[i * 3 + 1]; // y
+      positions[i * 3 + 2] += particlesData.velocities[i * 3 + 2]; // z
+
+      // 중심으로부터 멀리 날아가면 다시 초기화
+      const dist = Math.sqrt(
+        positions[i * 3] ** 2 +
+          positions[i * 3 + 1] ** 2 +
+          positions[i * 3 + 2] ** 2
+      );
+      const maxDistance = 1000; // 파티클이 최대 거리를 기준으로 사라지는 값
+      const minProbability = 0.01; // 최소 제거 확률
+      const maxProbability = 0.99; // 최대 제거 확률
+
+      // 확률 계산: 거리 기반 확률 설정
+      const probability =
+        Math.pow(dist / maxDistance, 2) * (maxProbability - minProbability) +
+        minProbability;
+
+      if (Math.random() < probability / 2) {
+        positions[i * 3] = getRandomPositionFromCenter();
+        positions[i * 3 + 1] = getRandomPositionFromCenter();
+        positions[i * 3 + 2] = getRandomPositionFromCenter();
+      }
+    }
+    particles.current.geometry.attributes.position.needsUpdate = true;
+  });
+
+  return (
+    <points ref={particles}>
+      <bufferGeometry>
+        <bufferAttribute
+          attach="attributes-position"
+          count={count}
+          array={new Float32Array(particlesData.positions)}
+          itemSize={3}
+        />
+      </bufferGeometry>
+      <pointsMaterial size={0.1} color={color} transparent opacity={0.8} />
+    </points>
   );
 }
