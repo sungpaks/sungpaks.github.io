1636b3215e998c357add2da5eacd75cfef1a7a60
sungpaks <emforhs03150@gmail.com>
2025-01-12 21:49:41 +0900

    feat: 혜성 꼬리 방향 구현, 멀수록 파티클이 빨리 죽게 구현

--- src/pages/r3f-demo.tsx
+++ src/pages/r3f-demo.tsx
@@ -6,8 +6,21 @@ import {
   useLoader,
   useThree
 } from "@react-three/fiber";
-import React, { Suspense, useCallback } from "react";
-import { Color, Mesh, Points, TextureLoader } from "three";
+import React, {
+  MutableRefObject,
+  Ref,
+  RefObject,
+  Suspense,
+  useCallback
+} from "react";
+import {
+  Color,
+  Float32BufferAttribute,
+  Mesh,
+  Points,
+  TextureLoader,
+  Vector3
+} from "three";
 import Layout from "../components/Layout";
 // @ts-ignore
 import ceres from "../images/ceres.jpg"; // @ts-ignore
@@ -34,17 +47,13 @@ export default function R3FDemo({ location }: { location: Location }) {
             fov: 75,
             near: 0.1,
             far: 1000,
-            position: [0, 50, 75]
+            position: [0, 175, 0]
             // lookAt: () => [0, 0, 0]
           }}
         >
           <ambientLight intensity={1} />
           {/* <directionalLight position={[10, 10, 10]} /> */}
           <Sun />
-          {/* <mesh position={[0, -25, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-            <planeGeometry args={[1000, 1000]} />
-            <meshStandardMaterial color={"rgb(50,50,50)"} />
-          </mesh> */}
         </Canvas>
       </div>
     </Layout>
@@ -52,12 +61,13 @@ export default function R3FDemo({ location }: { location: Location }) {
 }
 
 function Sun() {
-  const mesh = React.useRef<Mesh>(null);
+  const mesh = React.useRef<Mesh | null>(null);
   const background = useLoader(TextureLoader, starBackground);
   const sunTexture = useLoader(TextureLoader, sun);
 
-  useThree(({ scene }) => {
+  useThree(({ scene, camera }) => {
     scene.background = background;
+    camera.position.x = 20;
   });
 
   return (
@@ -77,29 +87,70 @@ function Sun() {
 }
 
 function SunSystem(props: MeshProps) {
+  let lastElapsedTime = 0;
+  const OrbitAfterImageArray: any[] = [];
+  const planetRef = React.useRef<Mesh | null>(null);
+  const trailCount = 30;
+  const positions = React.useRef<Float32Array>(
+    new Float32Array(trailCount * 3)
+  );
+  const pointIndex = React.useRef(0);
+  const trailRef = React.useRef<Points | null>(null);
+
+  useFrame(({ clock }) => {
+    const interval = clock.getElapsedTime() - lastElapsedTime;
+    if (interval > 1 && planetRef.current) {
+      lastElapsedTime = clock.getElapsedTime();
+
+      const planetPosition = new Vector3();
+      planetRef.current.getWorldPosition(planetPosition);
+
+      positions.current[pointIndex.current * 3] = planetPosition.x;
+      positions.current[pointIndex.current * 3 + 1] = planetPosition.y;
+      positions.current[pointIndex.current * 3 + 2] = planetPosition.z;
+
+      pointIndex.current = (pointIndex.current + 1) % trailCount;
+
+      if (trailRef.current) {
+        const geometry = trailRef.current.geometry;
+        geometry.setAttribute(
+          "position",
+          new Float32BufferAttribute(positions.current, 3)
+        );
+        trailRef.current.geometry.attributes.position.needsUpdate = true;
+      }
+    }
+  });
   return (
     <mesh {...props}>
       <Suspense fallback={null}>
-        <Planet position={[30, 0, 0]} />
+        <Planet planetRef={planetRef} position={[30, 0, 0]} />
       </Suspense>
+      <points ref={trailRef}>
+        <bufferGeometry />
+        <pointsMaterial size={1} color={"aqua"} />
+      </points>
     </mesh>
   );
 }
 
-function Planet(props: MeshProps) {
+interface PlanetProps extends MeshProps {
+  planetRef: MutableRefObject<Mesh | null>;
+}
+
+const Planet = ({ planetRef, ...props }: PlanetProps) => {
   const [isClicked, setIsClicked] = React.useState(false);
-  const mesh = React.useRef<Mesh>(null);
-  const [distanceToCenter, setDistanceToCenter] = React.useState(0);
+  const mesh = React.useRef<Mesh | null>(null);
 
   const texture = useLoader(TextureLoader, ceres);
 
   // 궤도 파라미터 설정
-  const semiMajorAxis = 60; // 장축 반지름 (x축 방향)
-  const semiMinorAxis = 20; // 단축 반지름 (y축 방향)
+  const semiMajorAxis = 100; // 장축 반지름 (x축 방향)
+  const semiMinorAxis = 40; // 단축 반지름 (z축 방향)
   const orbitCenter = [semiMajorAxis / 2, 0, 0]; // 중심점
-  const orbitSpeed = 0.5; // 속도
+  const orbitSpeed = 0.2; // 속도
 
-  useFrame(({ clock }) => {
+  useFrame(({ clock, scene }) => {
     if (!mesh.current) return;
     const elapsedTime = clock.getElapsedTime() * orbitSpeed; // 시간에 따라 궤도 위치 갱신
     mesh.current.lookAt(0, 0, 0); // 중심을 바라보도록 설정
@@ -113,14 +164,14 @@ function Planet(props: MeshProps) {
       orbitCenter[1],
       orbitCenter[2] + z
     );
-    // setDistanceToCenter(
-    //   Math.sqrt(mesh.current.position.x ** 2 + mesh.current.position.z ** 2)
-    // );
   });
 
   return (
     <mesh
-      ref={mesh}
+      ref={node => {
+        mesh.current = node;
+        planetRef.current = node;
+      }}
       onClick={() => setIsClicked(prev => !prev)}
       scale={isClicked ? 1.5 : 1}
       {...props}
@@ -128,30 +179,48 @@ function Planet(props: MeshProps) {
       <axesHelper args={[5]} />
       <sphereGeometry args={[1, 32, 32]} />
       <meshStandardMaterial args={[{ map: texture }]} />
-      <ParticleSystem distanceToCenter={distanceToCenter} color="#7DF9FF" />
       <ParticleSystem
-        distanceToCenter={distanceToCenter}
+        orbitSpeed={orbitSpeed}
+        color="#7DF9FF"
+        orbitCenter={orbitCenter}
+        planetRef={mesh}
+      />
+      <ParticleSystem
+        orbitSpeed={orbitSpeed}
         dust
         color="#FFD700"
+        orbitCenter={orbitCenter}
+        planetRef={mesh}
       />
       <ParticleSystem
-        distanceToCenter={distanceToCenter}
+        orbitSpeed={orbitSpeed}
         dust
         color="#FF4500"
+        orbitCenter={orbitCenter}
+        planetRef={mesh}
       />
-      <ParticleSystem distanceToCenter={distanceToCenter} color="#6457A6" />
+      <ParticleSystem
+        orbitSpeed={orbitSpeed}
+        color="#6457A6"
+        orbitCenter={orbitCenter}
+        planetRef={mesh}
+      />
     </mesh>
   );
-}
+};
 
 function ParticleSystem({
   color,
   dust,
-  distanceToCenter
+  orbitSpeed,
+  orbitCenter,
+  planetRef
 }: {
   color: string;
   dust?: boolean;
-  distanceToCenter: number;
+  orbitSpeed: number;
+  orbitCenter: number[];
+  planetRef: RefObject<Mesh | null>;
 }) {
   const particles = React.useRef<Points>(null);
   const getRandomPositionFromCenter = useCallback(() => {
@@ -164,24 +233,35 @@ function ParticleSystem({
     const velocities = [];
     for (let i = 0; i < count; i++) {
       const distance = 5 + Math.random() * 5; // 중심으로부터 거리
-      const x = dust ? (Math.random() - 0.9) * 4 : (Math.random() - 0.5) * 2;
-      const y = dust ? (Math.random() - 0.9) * 4 : (Math.random() - 0.5) * 2;
+      const x = dust ? (Math.random() - 0.9) * 5 : (Math.random() - 0.5) * 1.5;
+      const y = (Math.random() - 0.5) * 1.5;
       const z = Math.cos(Math.PI * 2) * -distance; // 높이 축 랜덤
       positions.push(
         getRandomPositionFromCenter(),
         getRandomPositionFromCenter(),
         getRandomPositionFromCenter()
       ); // 위치 설정 );
-      velocities.push(x * 0.01, y * 0.01, z * 0.01); // 속도 설정
+      const velocityFactor = dust ? 0.005 : 0.01;
+      velocities.push(
+        x * velocityFactor,
+        y * velocityFactor,
+        z * velocityFactor
+      ); // 속도 설정
     }
     return { positions, velocities };
   }, []);
   useFrame(() => {
     if (!particles.current) return;
+    const planetWorldPosition = new Vector3();
+    planetRef?.current?.getWorldPosition(planetWorldPosition);
+    const distanceFromCenter = Math.sqrt(
+      (planetWorldPosition.x - 2 * orbitCenter[0]) ** 2 +
+        (planetWorldPosition.z - 2 * orbitCenter[2]) ** 2
+    );
 
     const positions = particles.current.geometry.attributes.position.array;
     for (let i = 0; i < count; i++) {
-      positions[i * 3] += particlesData.velocities[i * 3] - (dust ? 0.01 : 0); // x
+      positions[i * 3] += particlesData.velocities[i * 3]; // x
       positions[i * 3 + 1] += particlesData.velocities[i * 3 + 1]; // y
       positions[i * 3 + 2] += particlesData.velocities[i * 3 + 2]; // z
 
@@ -191,7 +271,11 @@ function ParticleSystem({
           positions[i * 3 + 1] ** 2 +
           positions[i * 3 + 2] ** 2
       );
-      const maxDistance = 1000; // 파티클이 최대 거리를 기준으로 사라지는 값
+      if (dust) {
+        positions[i * 3] -= dist * orbitSpeed * 0.01;
+      }
+
+      const maxDistance = (distanceFromCenter / 100) ** 2 * 100; // 파티클이 최대 거리를 기준으로 사라지는 값
       const minProbability = 0.01; // 최소 제거 확률
       const maxProbability = 0.99; // 최대 제거 확률
 
